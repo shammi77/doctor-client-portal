a1441a431b369a20ffaa9df7fe8e7108233e9a52
shammi77 <nusratshammi.vutu@gmail.com>
2022-08-02 20:04:32 +0600

    last commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-
-import './App.css';
-import Navbar from './pages/Shared/Navbar';
+import "./App.css";
+import Navbar from "./pages/Shared/Navbar";
 import { Routes, Route } from "react-router-dom";
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Login from './pages/Login/Login';
-import NotFound from './pages/NotFound/NotFound';
-import Appointment from './pages/Appointment/Appointment';
-
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import NotFound from "./pages/NotFound/NotFound";
+import Appointment from "./pages/Appointment/Appointment";
+import SignUp from "./pages/Login/SignUp";
+import RequireAuth from "./pages/Login/RequireAuth";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import MyAppointments from "./pages/Dashboard/MyAppointments";
+import MyReview from "./pages/Dashboard/MyReview";
+import MyHistory from "./pages/Dashboard/MyHistory";
+import Users from "./pages/Dashboard/Users";
 
 function App() {
   return (
-    <div  className='max-w-7xl mx-auto px-12'>
-      
+    <div className="max-w-7xl mx-auto px-12">
       <Navbar></Navbar>
       <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="appointment" element={<Appointment />} />
+        <Route
+          path="appointment"
+          element={
+            <RequireAuth>
+              <Appointment />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="dashboard"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }>
+            <Route index element={<MyAppointments></MyAppointments>}></Route>
+            <Route path="review" element={<MyReview></MyReview>}></Route>
+            <Route path="history" element={<MyHistory></MyHistory>}></Route>
+            <Route path="users" element={<Users></Users>}></Route>
+          </Route>
+       
         <Route path="login" element={<Login />} />
-        <Route path='*' element={<NotFound></NotFound>}></Route>
+        <Route path="signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
